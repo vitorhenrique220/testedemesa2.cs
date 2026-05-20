b535b9bcf49f113a2578b41cb20e149d797a755c
vitorhenrique220 <vitorhen09@gmail.com>
2026-05-20 17:10:16 -0300

    Adicionar arquivos de projeto.

---
+++ testedemesa.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        decimal valorPresente = 3800m;
+        decimal taxaJuros = 1.25m / 100;
+        int periodoMes = 6;
+
+        double rendaAcumulada = (double)valorPresente;
+
+        for (int i = 0; i <= periodoMes; i++)
+
+        {
+            double rendimento =
+                (double)valorPresente * Math.Pow((double)(1 + taxaJuros), i);
+
+            double rendimentoLiquido = rendimento - (double)valorPresente;
+
+            rendaAcumulada += rendimentoLiquido;
+
+            Console.WriteLine("Mês: " + i);
+            Console.WriteLine("Rendimento: " + rendimento.ToString("C2"));
+            Console.WriteLine("Rendimento Líquido: " + rendimentoLiquido.ToString("C2"));
+            Console.WriteLine("Renda Acumulada: " + rendaAcumulada.ToString("C2"));
+           
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
